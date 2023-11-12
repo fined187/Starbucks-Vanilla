c902afab43963e19ec35941d83a6e441620c2bba
fined000@naver.com <fined000@naver.com>
2023-11-12 20:17:51 +0900

    swiper js

--- js/main.js
+++ js/main.js
@@ -35,3 +35,36 @@ window.addEventListener('scroll', _.throttle(function () {
 }, 300));
 
 // _.throttle(함수, 시간)
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,  //  0.7, 1.4, 2.1, 2.7
+    opacity: 1
+  })
+});
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
+new Swiper('.promotion .swiper-container', {
+  // direction: 'horizontal', 기본값
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 4000 // 4초
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev', // 이전 슬라이드 버튼
+    nextEl: '.promotion .swiper-next' // 다음 슬라이드 버튼
+  }
+});
