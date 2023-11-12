0efb03ecd68f6eb63fb2679eaf8dd57ca03ea9d9
fined000@naver.com <fined000@naver.com>
2023-11-12 22:04:24 +0900

    test

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ searchInputEl.addEventListener('blur', function () {
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function () {
-  console.log(window.scrollY);
   if (window.scrollY > 500) {
     // 배지 숨기기
     gsap.to(badgeEl, .6, {
@@ -66,5 +65,19 @@ new Swiper('.promotion .swiper-container', {
   navigation: {
     prevEl: '.promotion .swiper-prev', // 이전 슬라이드 버튼
     nextEl: '.promotion .swiper-next' // 다음 슬라이드 버튼
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add('hide');
+  } else {
+    // 보임 처리
+    promotionEl.classList.remove('hide');
   }
 });
